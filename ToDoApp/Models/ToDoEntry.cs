551310c0484f7e75f90d09f44b8f046fcb589082
Temitope Olufohunsi <temitope@olufohunsi.com>
2022-07-03 13:40:09 +0100

    Created ToDo controller and ToDoEntry model

---
+++ ToDoApp/Models/ToDoEntry.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Models
+{
+    public class ToDoEntry
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(50)]
+        public string? EntryText { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [Required]
+        public DateTime CreatedBy { get; set; }
+
+        public DateTime? ExpiresBy { get; set; }
+    }
+}
